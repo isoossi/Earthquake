523143c76ce4aef42cdbd5e6d2b3360d00f1c839
mikkoast <mikko.astrom@bitbar.com>
2018-01-18 22:55:37 -0800

    updated to work

--- app/src/main/java/com/example/test/earthquake/RetrieveJson.java
+++ app/src/main/java/com/example/test/earthquake/RetrieveJson.java
@@ -3,33 +3,31 @@ package com.example.test.earthquake;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.google.android.gms.maps.model.LatLng;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-//import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+
+//import java.util.Date;
 
 /**
  * Created by test on 25.5.2016.
  */
 public class RetrieveJson extends AsyncTask<String, Void, ArrayList<String>> {
-    URLConnection httpclient;
+    HttpURLConnection httpclient = null;
+    BufferedReader reader = null;
     Observer o = null;
-    StringBuilder result = new StringBuilder();
-    String result2 = "";
+    String result2 = null;
     static JSONObject properties = null;
     static JSONObject point = null;
     ArrayList<String> list = new ArrayList<>();
@@ -47,25 +45,41 @@ public class RetrieveJson extends AsyncTask<String, Void, ArrayList<String>> {
     @Override
     protected ArrayList<String> doInBackground(String... params) {
         try {
-            URL url = new URL("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson");
-            httpclient = url.openConnection();
-            InputStream in = new BufferedInputStream(httpclient.getInputStream());
+            URL url = new URL("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson");
+            httpclient = (HttpURLConnection) url.openConnection();
+            //httpclient.setRequestMethod("GET");
+            httpclient.connect();
+            int test = httpclient.getContentLength();
+            // Read the input stream into a String
+            InputStream in = httpclient.getInputStream();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "utf-8"),8 );
+            StringBuffer buffer = new StringBuffer();
+            if (in == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(in, "utf-8"), 8);
 
             String line;
             while ((line = reader.readLine()) != null) {
-                result.append(line+ "\n");
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
             }
-            in.close();
-            result2 = result.toString();
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            result2 = buffer.toString();
 
         } catch (Exception e) {
             Log.e("StringBuilding", "Error converting result " + e.toString());
         }
 
         try{
-            JSONObject jsonobjekt= new JSONObject(result2);
+            JSONObject jsonobjekt = new JSONObject(result2);
 
             //finding magnitude, place name, time and coordinates from json
             for (int i = 0; i < 1; i++) {
@@ -78,27 +92,41 @@ public class RetrieveJson extends AsyncTask<String, Void, ArrayList<String>> {
 
                         for (int k = 0; k < 1 ; k++) {
                             try {
-                                String value = properties.getString("mag");
-                                double max = Double.parseDouble(value);
                                 String mag = properties.getString("mag");
-                                list.add(mag);
+                                if(!mag.equals("null")) {
+                                    list.add(mag);
+                                } else {
+                                    list.add("No data");
+                                }
                                 String place = properties.getString("place");
-                                list.add(place);
+                                if(!place.equals("null")) {
+                                    list.add(place);
+                                } else {
+                                    list.add("No data");
+                                }
                                 String time = properties.getString("time");
-                                long timel = Long.parseLong(time);
-                                String format = "yyyy-MM-dd HH:mm:ss";
-                                SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
-                                sdf.setTimeZone(TimeZone.getDefault());
-                                String newtime = sdf.format(new Date(timel));
-                                list.add(newtime);
+                                if(!time.equals("null")) {
+                                    long timel = Long.parseLong(time);
+                                    String format = "yyyy-MM-dd HH:mm:ss";
+                                    SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
+                                    sdf.setTimeZone(TimeZone.getDefault());
+                                    String newtime = sdf.format(new Date(timel));
+                                    list.add(newtime);
+                                } else {
+                                    list.add("No data");
+                                }
                                 String coordinates = point.getString("coordinates");
-                                String test[] = coordinates.split("\\[");
-                                String testi = test[1];
-                                String test2[] = testi.split(",");
-                                String first = test2[0];
-                                list.add(first);
-                                String second = test2[1];
-                                list.add(second);
+                                if(!coordinates.equals("null")) {
+                                    String test[] = coordinates.split("\\[");
+                                    String testi = test[1];
+                                    String test2[] = testi.split(",");
+                                    String first = test2[0];
+                                    list.add(first);
+                                    String second = test2[1];
+                                    list.add(second);
+                                } else {
+                                    list.add("No data");
+                                }
                                 Log.e("JSON", "> " + mag + place + time + coordinates );
                             } catch (JSONException e) {
                                 e.printStackTrace();
